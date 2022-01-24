4921a4c768d44d904dc22075ba7c89cf9429abfe
Andres Mareca <andres.mareca@gmail.com>
2022-01-24 11:57:16 +0100

    Fix typo + add bool type

--- types/types.go
+++ types/types.go
@@ -8,12 +8,14 @@ import (
 type FieldType reflect.Type
 
 var (
-	StingType              = FieldType(reflect.TypeOf((*string)(nil)).Elem())
-	StingPtrType           = FieldType(reflect.TypeOf((*string)(nil)))
+	StringType             = FieldType(reflect.TypeOf((*string)(nil)).Elem())
+	StringPtrType          = FieldType(reflect.TypeOf((*string)(nil)))
 	IntegerType            = FieldType(reflect.TypeOf((*int)(nil)).Elem())
 	IntegerPtrType         = FieldType(reflect.TypeOf((*int)(nil)))
 	UnsignedIntegerType    = FieldType(reflect.TypeOf((*uint)(nil)).Elem())
 	UnsignedIntegerPtrType = FieldType(reflect.TypeOf((*uint)(nil)))
 	TimeType               = FieldType(reflect.TypeOf((*time.Time)(nil)).Elem())
 	TimePrtType            = FieldType(reflect.TypeOf((*time.Time)(nil)))
+	BooleanType            = FieldType(reflect.TypeOf((*bool)(nil)).Elem())
+	BooleanPrtType         = FieldType(reflect.TypeOf((*bool)(nil)))
 )
