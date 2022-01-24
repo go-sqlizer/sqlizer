c6e48b725436f1ce145b29bf3a30c746ffa6ca40
Andres Mareca <andres.mareca@gmail.com>
2022-01-24 11:16:03 +0100

    Add more dataTypes

--- types/types.go
+++ types/types.go
@@ -2,14 +2,18 @@ package types
 
 import (
 	"reflect"
+	"time"
 )
 
 type FieldType reflect.Type
 
 var (
-	StingType           = FieldType(reflect.TypeOf((*string)(nil)).Elem())
-	StingPtrType        = FieldType(reflect.TypeOf((*string)(nil)))
-	IntegerType         = FieldType(reflect.TypeOf((*int)(nil)).Elem())
-	IntegerPtrType      = FieldType(reflect.TypeOf((*int)(nil)))
-	UnsignedIntegerType = FieldType(reflect.TypeOf((*uint)(nil)).Elem())
+	StingType              = FieldType(reflect.TypeOf((*string)(nil)).Elem())
+	StingPtrType           = FieldType(reflect.TypeOf((*string)(nil)))
+	IntegerType            = FieldType(reflect.TypeOf((*int)(nil)).Elem())
+	IntegerPtrType         = FieldType(reflect.TypeOf((*int)(nil)))
+	UnsignedIntegerType    = FieldType(reflect.TypeOf((*uint)(nil)).Elem())
+	UnsignedIntegerPtrType = FieldType(reflect.TypeOf((*uint)(nil)))
+	TimeType               = FieldType(reflect.TypeOf((*time.Time)(nil)).Elem())
+	TimePrtType            = FieldType(reflect.TypeOf((*time.Time)(nil)))
 )
